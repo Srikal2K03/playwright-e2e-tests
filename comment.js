ae0e9f8016c0e751cd45200a13a660b804234cec
Srikanth <srikal2k03@gmail.com>
2026-02-19 00:36:22 +0530

    first commit

---
+++ comment.js
@@ -0,0 +1,15 @@
+// my first comment
+console.log('Hello World');
+
+
+/**
+ * 
+ * @param {npm init playwright@latest} num1 
+ * @param {*} num2 
+ * @returns 
+ */
+
+function  sum(num1, num2) 
+{
+    return num1+num2
+}
